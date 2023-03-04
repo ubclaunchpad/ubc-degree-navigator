7feec52d9f3ae393f71d0eb04a7631e8a079af34
james-magee <jam020201@gmail.com>
2023-03-04 13:48:09 -0800

    changed

--- go/src/db.go
+++ go/src/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 type User struct {
-	UserID uint `json:"userid"`
+	UserID uint `gorm:"primaryKey"`
 	Email string `json:"email`
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -15,7 +15,8 @@ type User struct {
 }
 
 type Course struct {
-	Faculty string `json:"faculty"`
+	CourseID uint `gorm:"primaryKey"`
+	Faculty string `json:"faculty"`		// Courses w/o Level are non-specific
 	Level uint `json:"level"` 				// first digit
 	CourseNum uint `json:"number"`
 }
@@ -28,9 +29,12 @@ type Program struct {
 }
 
 type Requirement struct {
-	Id string `json:"reqid"`
+	Id string `gorm:"primaryKey"`
+	Name string `json:"reqname"`
 	Description string `json:"desc"`
-	possibleCourses []Course `json:"courses"`
+	CoursesPermitted []Course `json:"coursesPermitted"`
+	CoursesExcepted []Course `json:"coursesExcepted"` // lets us use "3**"
+	Subreqs []Requirement `json:"subreqs"`        // if theres a link, check sub reqs
 	Credits uint `json:"credits"`
 }
 
@@ -50,6 +54,52 @@ func dbReturn(user *User) {
 	user.UserID = u.UserID
 }
 
+var db *gorm.DB
+
+func main() {
+	db, _ := gorm.Open("sqlite", "./gorm.db")
+	defer db.Close()
+	db.AutoMigrate(&Requirement)
+	db.AutoMigrate(&Course)
+	c1 := Course{Faculty: "MATH", Level: "*", CourseNum: "*"}
+	c2 := Course{Faculty: "MATH", Level: "3", CourseNum: "302"}
+
+
+
+	science_breadth := Requirement{
+		Name: "science_breadth", 
+		Description: "Science Breadth", 
+		Subreqs[]: {
+			Requirement{
+				Name: "math_breadth",
+				Description: "Math Subrequirement of Science-Breadth",
+				CoursesPermitted[]: {c1},
+				CoursesExcepted[]: {c2},
+				Credits: 3
+			}
+		},
+			// Requirement{
+			// 	Name: "science_breadth_subreq_chem_breadth",
+			// 	Description: "Chemistry Subrequirement of Science-Breadth",
+			// 	CoursesPermitted: [Course{Faculty: "CHEM", Level: "*", CourseNum: "*"}],
+			// 	CoursesExcepted: [Course{Faculty: "CHEM", Level: "1", CourseNum: "100"}, Course{Faculty: "CHEM", Level: "3", CourseNum: "300"}],
+			// 	Credits: 3
+			// },
+			// Requirement{
+			// 	Name: "science_breadth_subreq_phys_breadth",
+			// 	Description: "Physics Subrequirement of Science-Breadth",
+			// 	CoursesPermitted: [Course{Faculty: "CHEM", Level: "*", CourseNum: "***"}],
+			// 	CoursesExcepted: [Course{Faculty: "PHYS", Level: "1", CourseNum: "100"}],
+			// 	Credits: 3
+			// }
+		Credits: 6
+	}
+	db.Create(&c1)
+	db.Create(&c2)
+	db.Create(&science_breadth)
+}
+
+
 // func dbAccess() {
 // 	db, _ := gorm.Open("sqlite3", "./gorm.db")
 // 	defer db.Close()
@@ -66,3 +116,5 @@ func dbReturn(user *User) {
 // 	db.First(&u2)
 // 	fmt.Printf("UserID: %d, Username: %s, Email: %s, IntendedMajor: %s\n", u2.UserID, u2.Username, u2.Email, u2.IntendedMajor)
 // }
+
+
